79dd0422905a96090b0e8bacf6dd196e36713446
Stephen Fromm <stephenf@nero.net>
2012-08-17 10:20:59 -0700

    Add license and copyright information; add another documentation url to gnt-cobbler

--- gnt-cobbler.py
+++ gnt-cobbler.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
+# Written by Stephen Fromm <stephenf nero net>
+# (C) 2012 University of Oregon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 import xmlrpclib
 from optparse import OptionParser, OptionValueError
 import sys
 import getpass
 import socket
 
-# Reference
+# References
+# https://github.com/cobbler/cobbler/wiki/XMLRPC
 # http://jmrodri.fedorapeople.org/cobbler-integration.pdf
 
 def system_exists(name, server):
